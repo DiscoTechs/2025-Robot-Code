d6429251e41a0dabb252c0e3fb372424bc3cb188
Disco <team1099@brookfieldps.org>
2025-02-13 19:50:21 -0500

    Added Coral Command & Effector

---
+++ src/main/java/frc/robot/subsystems/CoralEffector.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class CoralEffector extends SubsystemBase {
+
+  private final SparkMax intakeMotor;
+
+  /** Creates a new AlgaeEffector. */
+  public CoralEffector() {
+    intakeMotor = new SparkMax(Constants.DriveConstants.kCoralEffectorMotorPort, MotorType.kBrushless);
+  }
+
+  public void intake() {
+    intakeMotor.set(0.5);
+  }
+
+  public void expel() {
+    intakeMotor.set(-0.5);
+  }
+
+  public void stop() {
+    intakeMotor.set(0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
