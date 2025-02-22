0c87b42c18d9c2226791cb0187ccb365add4c04d
Arjun Parimi <aparimi1204@gmail.com>
2025-02-22 18:07:45 -0500

    Added Climber

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Climber extends SubsystemBase{
+    private final SparkMax climbMotor;
+    
+    public Climber() {
+        climbMotor = new SparkMax(Constants.DriveConstants.kClimberMotorPort, MotorType.kBrushless);
+    }
+
+    public void climberUp() {
+        climbMotor.set(0.5); //temporary --> have to change to closedLoop
+    }
+
+    public void climberDown() {
+        climbMotor.set(-0.5); //temporary --> have to change to closedLoop
+    }
+
+    public void climberStop() {
+        climbMotor.set(0);
+    }
+}
