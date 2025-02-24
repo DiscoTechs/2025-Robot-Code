27021d22472b258458420b1275e887dfd1b642e1
Arjun Parimi <aparimi1204@gmail.com>
2025-02-24 13:24:09 -0500

    Added CoralPlateAngle SubSystem And Command

---
+++ src/main/java/frc/robot/commands/CoralPlateAngleCommand.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralPlateAngle;
+import frc.robot.Constants;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class CoralPlateAngleCommand extends Command {
+
+  private final CoralPlateAngle coralPlateAngle;
+  private final Joystick stick;
+
+  /** Creates a new CoralCommand. */
+  public CoralPlateAngleCommand(CoralPlateAngle coralPlateAngle, Joystick stick) {
+
+    this.coralPlateAngle = coralPlateAngle;
+    this.stick = stick;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(coralPlateAngle);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(stick.getRawButton(Constants.CoralConstants.CORAL_ANGLE_UP)) {
+      coralPlateAngle.angleUp();
+    } else if (stick.getRawButton(Constants.CoralConstants.CORAL_ANGLE_DOWN)) {
+        coralPlateAngle.angleDown();
+    } else {
+        coralPlateAngle.stopAngle();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/CoralPlateAngle.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class CoralPlateAngle extends SubsystemBase {
+
+  private final SparkMax angleMotor;
+
+  /** Creates a new CoralAngle. */
+  public CoralPlateAngle() {
+    angleMotor = new SparkMax(Constants.DriveConstants.kCoralPlateAngleMotorPort, MotorType.kBrushless);
+  }
+ 
+  public void angleUp() {
+    angleMotor.set(0.3); //temporary --> have to change to closedLoop/on-axel encoder
+  }
+
+  public void angleDown() {
+    angleMotor.set(-0.3); //temporary --> have to change to closedLoop/on-axel encoder
+  }
+
+  public void stopAngle()  {
+    angleMotor.set(0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
